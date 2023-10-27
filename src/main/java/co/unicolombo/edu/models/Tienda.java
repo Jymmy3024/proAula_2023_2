4098c03bacab231f26de7ab273e0120f895d928b
chasaj <jimmy.chaverra@unicolombo.edu.co>
2023-10-27 11:57:37 -0500

    paquetes, modelo, repositorio y servicio de tienda

---
+++ src/main/java/co/unicolombo/edu/models/Tienda.java
@@ -0,0 +1,43 @@
+package co.unicolombo.edu.models;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tiendas", schema = "dbo", catalog = "proAula_6")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tienda implements Serializable{
+    
+    private static final long SerialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo", nullable = false)
+    private int codigo;
+    @Column(name = "descripcion", nullable = true, length = 200)
+    private String descripcion;
+    @Column(name = "nombre", nullable = false, length = 45)
+    @NotBlank
+    @NotEmpty
+    private String nombre;
+    @Column(name = "ruta_imagen", nullable = true, length = 200)
+    private String ruta_imagen;
+    @Column(name = "nit", nullable = false)
+    @NotBlank
+    @NotEmpty
+    private int nit;
+    @Column(name = "tipo", nullable = true, length = 15)
+    private String tipo;
+}
---
+++ src/main/java/co/unicolombo/edu/services/TiendaServicioImp.java
@@ -0,0 +1,69 @@
+package co.unicolombo.edu.services;
+
+import co.unicolombo.edu.models.Tienda;
+import co.unicolombo.edu.repositories.TiendaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author jimmy
+ */
+public class TiendaServicioImp implements ITiendaServicio{
+    
+    @Autowired
+    private TiendaRepository tiendaRepo;
+    @Override
+    public List<Tienda> listarTiendas() throws Exception {
+        List<Tienda> listTienda = tiendaRepo.findAll();
+        if(listTienda == null && listTienda.isEmpty()){
+            throw new Exception("La lista de tiendas esta vacia.");
+        }else{
+         return listTienda;
+        }
+    }
+
+    @Override
+    public void agregarTienda(Tienda tienda) throws Exception {
+        if(tiendaRepo.existsById(tienda.getCodigo())){
+            throw new Exception("La Tienda con el codigo = " + tienda.getCodigo() + ", ya existe.");        
+        }else{
+            tiendaRepo.save(tienda);
+        }
+    }
+
+    @Override
+    public void editarTienda(Tienda tienda) throws Exception {
+        if(!tiendaRepo.existsById(tienda.getCodigo())){
+            throw new Exception("La Tienda que intenta editar con el codigo = " + tienda.getCodigo() + ", no existe.");        
+        }else{
+            tiendaRepo.save(tienda);
+        }
+    }
+
+    @Override
+    public void eliminarTienda(Tienda tienda) throws Exception {
+        Tienda tfind = tiendaRepo.findById(tienda.getCodigo()).orElse(null);
+        if(tfind == null){
+            throw new Exception("La Tienda que desea elminar No existe.");
+        }else{
+            tiendaRepo.delete(tienda);
+        }
+    }
+
+    @Override
+    public Tienda buscarTienda(Tienda tienda) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Tienda> buscarTiendaPorNombre(String nombre) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean existeTienda(Tienda tienda) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
